b4e67fe44d4e3539ff700bf33acafb9791af59b5
Johannes Maria Frank <jfrank@cloudian.com>
2020-06-14 02:10:55 +0100

    Gueesing game done

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,22 +1,40 @@
 extern crate rand;
 
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
 use std::io;
-use rand::{Rng, thread_rng};
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess");
 
-    println!("Please input your guess");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
+        }
+    }
 }
