741357b5ffd64aa6cd46f4b0372c7846db67a273
Johannes Frank <jmfrank63@gmail.com>
2020-12-16 13:08:28 +0000

    Input function working

--- user_interface/src/main.rs
+++ user_interface/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Result, stdin};
+
+fn main()-> Result<()>{
+    let mut buffer = String::new();
+    user_input("Please add a person to a department", &mut buffer)?;
+    println!("{}", buffer);
+    Ok(())
+}
+
+fn user_input<'a>(prompt: &str, buffer: &'a mut String) -> Result<&'a str> {
+    println!("{} :", prompt);
+    stdin().read_line(buffer)?;
+    Ok(buffer)
 }
