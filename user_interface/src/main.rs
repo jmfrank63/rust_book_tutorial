199b24ec29baac33e47fd9b353a7ad34c772bc50
Johannes Frank <jmfrank63@gmail.com>
2020-12-17 00:53:32 +0000

    Working intermediate after restart

--- user_interface/src/main.rs
+++ user_interface/src/main.rs
@@ -1,57 +1,27 @@
 use std::collections::HashMap;
+use std::convert::TryInto;
 use std::io::{stdin, Result};
 
 fn main() {
     let mut hmap: HashMap<&str, &str> = HashMap::new();
-    {
-        let buffer = loop {
-            let mut buffer = String::new();
-            match user_input("Please add a person to a department", &mut buffer) {
-                Ok(_) => break buffer,
-                Err(err) => {
-                    println!("{}", err);
-                    continue;
-                }
-            }
+    let mut input: String;
+
+    loop {
+        input = String::new();
+        stdin().read_line(&mut input).expect("Unable to get input!");
+        input = input.trim().to_owned();
+        if input.is_empty() {
+            break;
         };
-
-        let buffer = buffer.to_owned();
-        let command = parse_input(&buffer);
-        match &command[0].to_lowercase()[..] {
-            "add" => add_to_department(&mut hmap, command),
-            "remove" => remove_from_department(&mut hmap, command),
-            _ => println!("Unknown command {}", command[0]),
-        }
-
-        println!("{:?}", hmap);
+        let command = parse_input(&input[..]);
+        println!("{:?}", command);
     }
 }
 
-fn user_input<'a>(prompt: &str, buffer: &'a mut String) -> Result<&'a str> {
-    println!("{} :", prompt);
-    stdin().read_line(buffer)?;
-    if buffer.chars().any(char::is_numeric) {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidInput,
-            "No numbers are allowed!",
-        ))
-    } else {
-        Ok(buffer)
-    }
-}
-
-fn parse_input(buffer: &str) -> [&str; 3] {
-    let command: Vec<&str> = buffer.split(' ').collect();
-    // ignore "to" or "from"
-    [command[0], command[1], command[3].trim()]
-}
-
-fn add_to_department<'a>(hmap: &mut HashMap<&'a str, &'a str>, command: [&'a str; 3]) {
-    hmap.insert(command[2], command[1]);
-    println!("Added {:?}", command);
-}
-
-fn remove_from_department<'a>(hmap: &mut HashMap<&'a str, &'a str>, command: [&'a str; 3]) {
-    hmap.remove(command[2]);
-    println!("Removed {:?}", command);
+fn parse_input(input: &str) -> Result<[&str; 3]> {
+    let mut v: Vec<&str> = input.split(' ').collect();
+    v.remove(2);
+    let action :[&str;3] = v.try_into()
+    .unwrap_or_else(|v: Vec<&str>|{println!("Not good!");["","",""]});
+    Ok(action)
 }
